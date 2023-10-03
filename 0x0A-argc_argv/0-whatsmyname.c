c37523044cb12829a3d5df23398ccf1cd33dedaf
Pam-123 <pamschawira@gmail.com>
2023-10-03 03:38:16 -0700

    My Name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry of Program
- * argc - is the argument counter
- * argv - is the argument vector
- * Return (0)
+ * @argc: is the argument counter
+ * @argv: is the argument vector
+ * Return: (0)
  */
 
 int main(int argc, char *argv[])
