1e1420e511e49170c32e8b64f3aea6ab24f08949
Pam-123 <pamschawira@gmail.com>
2023-10-03 03:44:20 -0700

    Counting

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of program
+ * @argc: is the argument counter
+ * @argv: is the argument vector
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d \n", argc);
+	return (0);
+}
