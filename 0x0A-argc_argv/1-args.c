cf246f20db183e44dbcb094dbe5d5bfb1b158f03
Pam-123 <pamschawira@gmail.com>
2023-10-03 05:12:52 -0700

    Zero

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - Entry of program
+ * main - Entry of the Program
  * @argc: is the argument counter
  * @argv: is the argument vector
- * Return: (0)
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
+	int count = 0;
+	int i;
 	(void) argv;
 
-	printf("%d \n", argc - 1);
+	for (i = 1; i < argc; ++i)
+	{
+		count++;
+	}
+	if (count > 0)
+	{
+		printf("Arguments provided.\n");
+	}
+	else
+	{
+		printf("Arguments not provided.\n");
+	}
+
 	return (0);
 }
+
