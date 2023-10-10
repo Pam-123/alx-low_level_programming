44bd715f5e80d36e3b53ca53e01700bdacca071c
Pam-123 <pamschawira@gmail.com>
2023-10-09 20:23:18 -0700

    Dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+*README STARTS*
+
+* A composite data type used to group variables of different data types under a single name.
+* Allow you to create more complex data structures that can hold multiple pieces of information.
+
+*README ENDS*
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the owner's name
+ *
+ * Description: This structure represents information about a dog,
+ * including its name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
