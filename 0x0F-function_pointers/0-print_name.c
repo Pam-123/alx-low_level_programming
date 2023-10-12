6926a76d64a731b841581b9a6c15120b3625d5c9
Pam-123 <pamschawira@gmail.com>
2023-10-11 18:44:17 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,23 +1,38 @@
-#include "main.h"
+#include "function_pointers.h"
+#include <stddef.h>
+#include <unistd.h>
+
+/**
+ * _putchar - function to print
+ * @c: Variable
+ * Return: (write(1, &c, 1))
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_name - prints a name using a specified printing function
  * @name: the name to be printed
  * @f: the function pointer to the printing function
  */
-void print_name(char *name, void (*f)(char *)) {
-	if (name != NULL && f != NULL) {
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
 		f(name);
-	}
 }
 
 /**
  * print_name_with_newline - prints the name with a newline character
  * @name: the name to be printed
  */
-void print_name_with_newline(char *name) {
+void print_name_with_newline(char *name)
+{
 	int i = 0;
-	while (name[i] != '\0') {
+
+	while (name[i] != '\0')
+	{
 		_putchar(name[i]);
 		i++;
 	}
@@ -28,9 +43,12 @@ void print_name_with_newline(char *name) {
  * print_name_with_spaces - prints the name with spaces between characters
  * @name: the name to be printed
  */
-void print_name_with_spaces(char *name) {
+void print_name_with_spaces(char *name)
+{
 	int i = 0;
-	while (name[i] != '\0') {
+
+	while (name[i] != '\0')
+	{
 		_putchar(name[i]);
 		_putchar(' ');
 		i++;
