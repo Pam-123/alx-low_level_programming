64d3518f1368733c911105fa7ad31f52b908006e
Pam-123 <pamschawira@gmail.com>
2023-10-11 19:17:25 -0700

    OP

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,57 +1,14 @@
 #include "function_pointers.h"
-#include <stddef.h>
-#include <unistd.h>
 
 /**
- * _putchar - function to print
- * @c: Variable
- * Return: (write(1, &c, 1))
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_name - prints a name using a specified printing function
- * @name: the name to be printed
- * @f: the function pointer to the printing function
- */
+  * print_name - Prints a name
+  * @name: The name to prints
+  * @f: Pointer to function
+  *
+  * Return: Nothing
+  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 		f(name);
-}
-
-/**
- * print_name_with_newline - prints the name with a newline character
- * @name: the name to be printed
- */
-void print_name_with_newline(char *name)
-{
-	int i = 0;
-
-	while (name[i] != '\0')
-	{
-		_putchar(name[i]);
-		i++;
-	}
-	_putchar('\n');
-}
-
-/**
- * print_name_with_spaces - prints the name with spaces between characters
- * @name: the name to be printed
- */
-void print_name_with_spaces(char *name)
-{
-	int i = 0;
-
-	while (name[i] != '\0')
-	{
-		_putchar(name[i]);
-		_putchar(' ');
-		i++;
-	}
-	_putchar('\n');
 }
