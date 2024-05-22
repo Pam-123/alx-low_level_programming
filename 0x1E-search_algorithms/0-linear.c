9134bc3c81c754b67a214e9528905b5cd80cffcd
Pam-123 <pamschawira@gmail.com>
2024-05-22 11:57:43 -0700

    Completed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,32 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
-        return -1;
-    }
+// Prototype of linear_search function
+int linear_search(int *array, size_t size, int value);
 
-    for (size_t i = 0; i < size; i++) {
-        printf("Comparing value %d with %d\n", array[i], value);
-        if (array[i] == value) {
-            return i; // Return the index where the value is found
-        }
-    }
-    
-    return -1; // Value not found
-}
+int main(void) {
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
 
-int main() {
-    int arr[] = {4, 7, 2, 9, 1, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int value_to_find = 9;
-
-    int index = linear_search(arr, size, value_to_find);
-
-    if (index != -1) {
-        printf("The value %d was found at index %d.\n", value_to_find, index);
-    } else {
-        printf("The value %d was not found in the array.\n", value_to_find);
-    }
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
 
     return 0;
 }
+
